53cd6cb1506d966a30f097a3fdc5217a94b3f7ee
soumaya <soumaya.rabah68@uha.fr>
2025-11-28 11:24:24 +0100

    MAJ config

---
+++ database/config.py
@@ -0,0 +1,8 @@
+# Configuration de la base de données
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'routage_oignon',
+    'user': 'routage_user',
+    'password': 'wxcvbn§!',
+    'port': 3306
+}
